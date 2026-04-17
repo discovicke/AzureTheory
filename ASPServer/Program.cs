93be7bae507fad0f0c2d088e682782fde645d219
Christian Gennari <christiangennari61@gmail.com>
2026-04-17 07:54:57 +0000

    add connectionstring and containername for Azure Blob Storage

--- ASPServer/Program.cs
+++ ASPServer/Program.cs
@@ -3,7 +3,9 @@ using Azure.Storage.Blobs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration("");//lägg till
+var connectionString = builder.Configuration(
+    "DefaultEndpointsProtocol=https;AccountName=blobtester;AccountKey=FtFjO9g0WEV8SOJyI0rN5kZ5t2uRBTNRelbgNzCDJ0OW1Mk2jSMQz5aO+85I91KDl6ud8f75/pn7+AStrpzbKQ==;EndpointSuffix=core.windows.net"
+); //lägg till
 
 builder.Services.AddSingleton(new BlobServiceClient(connectionString));
 
@@ -14,26 +16,28 @@ app.UseStaticFiles();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/upload", async (BlobServiceClient blobServiceClient, IFormFile file) =>
-{
-    if (file == null || file.Length == 0)
+app.MapPost(
+    "/upload",
+    async (BlobServiceClient blobServiceClient, IFormFile file) =>
     {
-        return Results.BadRequest("No file");
+        if (file == null || file.Length == 0)
+        {
+            return Results.BadRequest("No file");
+        }
+
+        var containerName = "useruploads"; //lägg till!
+        var blobName = file.FileName;
+
+        var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        await containerClient.CreateIfNotExistsAsync();
+
+        var blobClient = containerClient.GetBlobClient(blobName);
+
+        using var stream = file.OpenReadStream();
+        await blobClient.UploadAsync(stream, overwrite: true);
+
+        return Results.Ok(new { blobName });
     }
-
-    var containerName = ""; //lägg till!
-    var blobName = file.FileName;
-
-    var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-    await containerClient.CreateIfNotExistsAsync();
-
-    var blobClient = containerClient.GetBlobClient(blobName);
-
-    using var stream = file.OpenReadStream();
-    await blobClient.UploadAsync(stream, overwrite: true);
-
-    return Results.Ok(new { blobName });
-
-});
+);
 
 app.Run();
